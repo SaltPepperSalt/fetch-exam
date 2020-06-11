a6133352f4f648070faad70845f995e0f3499384
SaltPepperSalt <dksro324@gmail.com>
2020-06-11 17:16:16 +0900

    fetch practice done

--- public/js/app.js
+++ public/js/app.js
@@ -1,9 +1,12 @@
-const $pre = document.querySelector('pre');
+//State
 let todos = [];
 
-const render = _todos => {
-  $pre.innerHTML = JSON.stringify(_todos, null, 2);
-}
+//DOM
+const $pre = document.querySelector('pre');
+const $inputTodo = document.querySelector('.input-todo');
+const $deleteTodo = document.querySelector('.delete-todo');
+const $toggleTodo = document.querySelector('.toggle-todo');
+
 
 
 const request = (function () {
@@ -33,7 +36,65 @@ const request = (function () {
   };
 })();
 
+const render = () => {
+  $pre.innerHTML = JSON.stringify(todos, null, 2)
+};
 
+const getId = () => {
+  return todos.length ? Math.max(...todos.map(todo => todo.id)) + 1 : 1;
+};
+
+const addTodo = content => {
+  request.post('/todos', { id: getId(), content, completed: false })
+  .then(response => response.json())
+  .then(todo => todos = [...todos, todo])
+  .then(() => render())
+  .catch(err => console.error(err));
+};
+
+const deleteTodo = id => {
+  request.delete(`/todos/${id}`)
+  .then(() => {
+    todos = todos.filter(todo => todo.id !== +id);
+    render();
+  })
+  .catch(err => console.error(err));
+}
+const toggleTodo = id => {
+  const completed = !todos.find(todo => todo.id === +id).completed;
+  request.patch(`/todos/${id}`, { completed })
+  .then(response => response.json())
+  .then(_todo => todos = todos.map(todo => todo.id === _todo.id ? _todo : todo))
+  .then(render)
+  .catch(err => console.error(err));
+}
+
+window.onload = () => {
+  console.log('hi');
+  request.get('/todos')
+  .then(response => response.json())
+  .then(_todos => {
+    todos = _todos;
+    render();
+  })
+  .catch(err => console.error(err));
+};
+
+$inputTodo.onkeyup = e => {
+  if(e.keyCode !== 13) return;
+  addTodo($inputTodo.value);
+  $inputTodo.value = '';
+};
+$deleteTodo.onkeyup = e => {
+  if(e.keyCode !== 13) return;
+  deleteTodo($deleteTodo.value);
+  $deleteTodo.value = '';
+}
+$toggleTodo.onkeyup = e => {
+  if(e.keyCode !== 13) return;
+  toggleTodo($toggleTodo.value);
+  $toggleTodo.value = '';
+}
 
 // fetch('/todos')
 //   .then(response => response.json())
